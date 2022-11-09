d7df9fe39cc610a382509b0183fdbac5f6d8bb7b
zrenox <112039336+zrenox@users.noreply.github.com>
2022-11-09 09:43:41 -0800

    My first commit!
    Dark DEM Model Lab

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//function to start the Leaflet map
+function createMap(){
+
+    //Varibles for attribution box on the map.
+    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' +' <a href="http://mapbox.com">Mapbox</a>'; //You can add you name to the variable if you want to add yourself to the credits.
+    
+    //Variable for storing your Mapbox API Token
+    var apitoken = 'pk.eyJ1IjoienJlbm94IiwiYSI6ImNsOWVzbG14bTBjeGIzb21zMng4dWZieTEifQ.KNDYVG_whYAQz4aK5gT-Pw' //Enter your API Token - go to 'https://www.mapbox.com/install/' to yours */
+    
+     //URL used for Stanard MaxBox Styles
+    var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}';
+    
+    //URL used for Custom MapBox Style
+    var mbStyleUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}';
+    
+    //For Custome basemaps - Replace your username and StyleID
+    var customeBasemap = L.tileLayer(mbStyleUrl, {id: 'zrenox/cl9esoz5m004c14lgoqwqe6tu', token: apitoken,  attribution: mbAttr});
+    
+    //For MabBox Standard Basemaps
+    var standardBasemap   = L.tileLayer(mbUrl, {id: 'mapbox.light', token: apitoken, attribution: mbAttr});
+    
+    //create the map*/
+    var map = L.map('map', {
+        center: [31.00, -5],//Coordinated to center the map
+        zoom: 6, //zoom level
+        layers:customeBasemap //basemap
+    });
+    
+};
+
+//calling the funcation
+$(document).ready(createMap);
